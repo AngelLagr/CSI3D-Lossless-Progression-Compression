e55d657abd34ba8fad25989e777dd7d9f7438a07
AngelLagr <119140988+AngelLagr@users.noreply.github.com>
2025-11-21 15:33:16 +0100

    tests

--- base_files/frenet_coordinate.py
+++
@@ -1,8 +0,0 @@
-class FrenetCoordinates:
-    def __init__(self, u: float, v: float, n: float):
-        self.tangent_x = u
-        self.tangent_y = v
-        self.normal_z = n
-
-    def apply(self, alpha: int, beta: int, gamma: int) -> np.ndarray:
-        return (self.tangent_x * alpha, self.tangent_y * beta, self.normal_z * gamma)
